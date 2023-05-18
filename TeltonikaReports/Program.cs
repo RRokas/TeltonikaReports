72cb1f9caac6d0c29b1f603dd34b0bfb04301c03
RRingaila <r.ringaila@mttc.eu>
2023-05-18 12:10:27 +0300

    Initial commit

---
+++ TeltonikaReports/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeltonikaReports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
