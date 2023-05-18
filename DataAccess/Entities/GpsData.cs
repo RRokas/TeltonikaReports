10cc0d50ad08061f45a1be6eee8d018086f4bf57
RRingaila <r.ringaila@mttc.eu>
2023-05-18 12:11:08 +0300

    GpsData entity definition

---
+++ DataAccess/Entities/GpsData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataAccess.Entities
+{
+    public class GpsData
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime GpsTime { get; set; }
+        public double Speed { get; set; }
+        public float Angle { get; set; }
+        public double Altitude { get; set; }
+        public int Satellites { get; set; }
+    }
+}
