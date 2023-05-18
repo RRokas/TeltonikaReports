896363255b4db70abc56c27a3c3ebd48095604d1
RRingaila <r.ringaila@mttc.eu>
2023-05-18 12:11:40 +0300

    naive implementation of JsonDataProvider

---
+++ DataAccess/DataSources/JsonDataProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using DataAccess.Entities;
+
+namespace DataAccess.DataProviders
+{
+    public static class JsonDataProvider
+    {
+        public static List<GpsData> GetData(string filepath)
+        {
+            var fileContent = File.ReadAllText(filepath);
+            var gpsDataLog = JsonSerializer.Deserialize<List<GpsData>>(fileContent);
+            return gpsDataLog;
+        } 
+    }
+}
