76f6dd5591776795738ca9c154ae432a8d6150a3
RRokas <rokas.ringaila@gmail.com>
2023-05-21 13:23:15 +0300

    methods for transforming data for reports

---
+++ TeltonikaReports/ReportChartDataGenerators.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataAccess.Entities;
+
+namespace TeltonikaReports
+{
+    public static class ReportChartDataGenerators
+    {
+        public static List<(string, int)> OrderedSatelliteCountHistogram(List<GpsData> sourceData)
+        {
+            return sourceData.GroupBy(gpsData => gpsData.Satellites)
+                .Select(group => (group.Key.ToString(), group.Count()))
+                .OrderBy(x=>x.Item1)
+                .ToList();
+        }
+
+        public static List<(string, int)> OrderedSpeedHistogram(List<GpsData> sourceData, int groupSize)
+        {
+            return sourceData.GroupBy(x => Math.Floor(x.Speed / groupSize) * groupSize)
+                .Select(group 
+                    => ($"{group.Min(gpsData => gpsData.Speed)}-{group.Max(gpsData => gpsData.Speed)}",
+                        group.Count()))
+                .ToList();
+        }
+    }
+}
