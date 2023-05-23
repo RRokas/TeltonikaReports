ebc672a73ba9cc5b99bc736abcfcee7d01f5d87e
RRokas <rokas.ringaila@gmail.com>
2023-05-23 22:15:54 +0300

    readability improvement: added names to tuple values

--- TeltonikaReports/ChartRenderer.cs
+++ TeltonikaReports/ChartRenderer.cs
@@ -6,19 +6,19 @@ namespace TeltonikaReports
 {
     public static class ChartRenderer
     {
-        public static void HorizontalChart(List<(string, int)> chartData)
+        public static void HorizontalChart(List<(string label, int value)> chartData)
         {
             const string labelSeparator = "|";
-            var maxPlainLabelLength = chartData.Select(x => x.Item1.Length).Max();
-            var highestValue = chartData.Select(data => data.Item2).Max();
+            var maxPlainLabelLength = chartData.Select(x => x.label.Length).Max();
+            var highestValue = chartData.Select(data => data.value).Max();
             var maxWidthForGraphCells = Console.WindowWidth - labelSeparator.Length - maxPlainLabelLength;
             
             var singleCellSize = Math.Ceiling((float)highestValue / (float)maxWidthForGraphCells);
 
             foreach (var data in chartData)
             {
-                var cellsToDraw = data.Item2 / singleCellSize;
-                var label = data.Item1.PadLeft(maxPlainLabelLength);
+                var cellsToDraw = data.value / singleCellSize;
+                var label = data.label.PadLeft(maxPlainLabelLength);
 
                 Console.Write(label+labelSeparator);
                 for (int drawnCells = 0; drawnCells < cellsToDraw; drawnCells++)
