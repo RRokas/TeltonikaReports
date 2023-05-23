7f601fe755bf9948655692c7de1cb800239e8a58
RRokas <rokas.ringaila@gmail.com>
2023-05-23 22:26:27 +0300

    Vertical chart renderer added

--- TeltonikaReports/ChartRenderer.cs
+++ TeltonikaReports/ChartRenderer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace TeltonikaReports
 {
@@ -27,6 +28,39 @@ namespace TeltonikaReports
                 }
                 Console.WriteLine();
             }
+        }
+
+        public static void VerticalChart(List<(string label, int value)> chartData)
+        {
+            var maxHeight = Console.WindowHeight - 2; // -1 offset for data labels, -1 for title,
+                                                        // -2 for accounting for process finish lines (if any);
+            var highestValue = chartData.Select(data => data.value).Max();
+            
+            var singleCellSize = Math.Ceiling((float)highestValue / (float)maxHeight);
+
+            var valuesConvertedToRows = chartData.Select(x => (x.label, x.value / (int)singleCellSize));
+
+            var graph = new StringBuilder();
+
+            graph.Append($"Highest value: {highestValue}".PadLeft(chartData.Count*3));
+            for (int row = maxHeight; row >= 0; row--)
+            {
+                foreach (var data in valuesConvertedToRows)
+                {
+                    graph.Append(data.Item2 >= row ? "██ " : "   ");
+                }
+                graph.AppendLine();
+            }
+            
+            foreach (var data in chartData)
+            {
+                var formattedLabel = data.label;
+                if (formattedLabel.Length < 2)
+                    formattedLabel = "0" + formattedLabel;
+                graph.Append($"{formattedLabel} ");
+            }
+            
+            Console.WriteLine(graph);
         }
     }
 }
