972b9eb1a17fb84958dae9684f9132b8f064d7e7
RRingaila <r.ringaila@mttc.eu>
2023-05-24 17:21:44 +0300

    Main method cleanup: separated each report into it's own function

--- TeltonikaReports/Program.cs
+++ TeltonikaReports/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using DataAccess;
 using DataAccess.Entities;
 
@@ -17,35 +16,58 @@ namespace TeltonikaReports
 
             var reportNumber = Console.ReadLine();
 
-            if (reportNumber == "1" || reportNumber == "2")
+            switch (reportNumber)
             {
-                Console.WriteLine("Enter path to file:");
-                var filepath = Console.ReadLine();
-                var gpsDataLog = new GpsDataRepository(filepath).GetAllData();
-                if (reportNumber == "1")
-                {
-                    Console.Clear();
-                    ChartRenderer.VerticalChart(ReportDataGenerators.OrderedSatelliteCountHistogramGraphData(gpsDataLog));
-                } else if (reportNumber == "2")
-                {
-                    Console.Clear();
-                    ChartRenderer.HorizontalChart(ReportDataGenerators.OrderedSpeedHistogram(gpsDataLog));
-                }
-            } else if (reportNumber == "3")
-            {
-                Console.Write("Directory path to source files (folder should contain only supported files):");
-                var directoryPath = Console.ReadLine();
-                var gpsDataLog = new List<GpsData>();
-                foreach (var file in System.IO.Directory.GetFiles(directoryPath))
-                {
-                    gpsDataLog.AddRange(new GpsDataRepository(file).GetAllData());
-                }
-                Console.Clear();
-                Console.WriteLine("Loading...");
-                var reportData = ReportDataGenerators.FindFastestSectionGraphData(gpsDataLog);
-                Console.Clear();
-                Console.Write(reportData);
+                case "1":
+                    SatelliteCountHistogramReport(AskForPath("Enter path to file:"));
+                    break;
+                case "2":
+                    SpeedHistogramReport(AskForPath("Enter path to file:"));
+                    break;
+                case "3":
+                    FastestSectionReport(AskForPath("Enter path to directory:"));
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
             }
+
+            Console.Read();
+        }
+
+        private static void SpeedHistogramReport(string filepath)
+        {
+            var gpsDataLog = new GpsDataRepository(filepath).GetAllData();
+            Console.Clear();
+            ChartRenderer.HorizontalChart(ReportDataGenerators.OrderedSpeedHistogram(gpsDataLog));
+        }
+        
+        private static void SatelliteCountHistogramReport(string filepath)
+        {
+            Console.Clear();
+            var gpsDataLog = new GpsDataRepository(filepath).GetAllData();
+            ChartRenderer.VerticalChart(ReportDataGenerators.OrderedSatelliteCountHistogramGraphData(gpsDataLog));
+        }
+        
+        private static void FastestSectionReport(string directoryPath)
+        {
+            Console.Clear();
+            var gpsDataLog = new List<GpsData>();
+            foreach (var file in System.IO.Directory.GetFiles(directoryPath))
+            {
+                gpsDataLog.AddRange(new GpsDataRepository(file).GetAllData());
+            }
+            
+            Console.WriteLine("Loading...");
+            var reportData = ReportDataGenerators.FindFastestSectionGraphData(gpsDataLog);
+            Console.Clear();
+            Console.Write(reportData);
+        }
+        
+        private static string AskForPath(string prompt)
+        {
+            Console.WriteLine(prompt);
+            return Console.ReadLine();
         }
     }
 }
